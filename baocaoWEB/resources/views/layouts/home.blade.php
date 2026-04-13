352900af0a57ed486d791331f6c95f7d9acae0d4
Phạm-27luan <luanp3987@gmail.com>
2026-04-13 20:03:05 +0700

    Cập nhật giao diện đặt vé

--- baocaoWEB/resources/views/layouts/home.blade.php
+++ baocaoWEB/resources/views/layouts/home.blade.php
@@ -8,9 +8,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,300;14..32,400;14..32,600;14..32,700;14..32,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <style>
-       
-    </style>
 </head>
 <body>
 
